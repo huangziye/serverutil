b7038381d467c2a5537314d528d4c31cc94c8afd
ziye_huang <ziye_huang@163.com>
2019-03-13 17:33:53 +0800

    功能添加。

--- src/main/java/com/hzy/StringUtil.kt
+++ src/main/java/com/hzy/StringUtil.kt
@@ -3,11 +3,16 @@ package com.hzy
 object StringUtil {
 
     /**
+     * 判断字符串是否为空
+     */
+    fun isEmpty(str: String) = "" == str
+
+    /**
      * 用自定义的分隔符拼接数据
      */
     fun <T> joinToString(
             collection: Collection<T>,
-            separator: String = ", ",
+            separator: String = ",",
             prefix: String = "",
             postfix: String = ""
     ): String {
